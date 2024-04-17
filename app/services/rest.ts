dbd637bfce9c70a6357c59008d85edc2fb64ecfc
dian afrial <agil.rahadi@gmail.com>
2024-04-17 23:13:21 +0700

    fix: headers & update stock modal

--- app/services/rest.ts
+++ app/services/rest.ts
@@ -2,15 +2,17 @@ import { createApi, fetchBaseQuery,  } from '@reduxjs/toolkit/query/react'
 import { BaseQueryFn } from '@reduxjs/toolkit/query/react'
 import { AppStore, store, RootState } from "./store"
 import { getUserToken } from './models/auth.model'
-
+ 
 export const baseQuery = fetchBaseQuery({ 
     baseUrl: import.meta.env.VITE_STAFF_API_URL,
+    mode:'cors',
     prepareHeaders: async (headers, { getState }) => {
         const auth = await getUserToken()
         //const token = (getState() as any)?.auth?.token
         // // If we have a token set in state, let's assume that we should be passing it.
-        headers.set("Access-Control-Allow-Origin", '*');
-        
+        //headers.append("Access-Control-Allow-Origin", '*');
+        headers.append("Content-Type", 'application/json');
+
         if (auth?.token) {
             headers.set('authorization', `Bearer ${auth?.token}`);
         }
